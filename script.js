547ac608dfc41fb544ff417de2fceea69bf914b2
ilde4 <46386810+ilde4@users.noreply.github.com>
2024-08-31 22:48:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pokeName = document.getElementById("pokemon-name");
 const pokeId = document.getElementById("pokemon-id");
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
-const pokeImg = document.getElementById("pokemon-img");
+const pokeImg = document.getElementById("pokemon-image");
 const types = document.getElementById("types");
 const hp = document.getElementById("hp");
 const attack = document.getElementById("attack");
@@ -15,6 +15,15 @@ const speed = document.getElementById("speed");
 
 let pokemonArr = [];
 let pokeStatsArr = [];
+
+const statsList = [
+    hp,
+    attack,
+    defense,
+    specialAttack,
+    specialDefense,
+    speed
+];
 
 
 fetch('https://pokeapi-proxy.freecodecamp.rocks/api/pokemon')
@@ -38,28 +47,48 @@ const isPokemon = (input) => {
     return false;
 }
 
-const fetchData = async (pokemon) => {
+const getPokemonStats = async (pokemon) => {
     const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemon}`);
     const data = await res.json();
+    const image = data.sprites.front_default;
+    const typeArr = data.types;
+    weight.innerHTML = `Weight: ${data.weight}`;
+    height.innerHTML = `Height: ${data.height}`;
+    pokeImg.innerHTML = `<img src="${image}" id="sprite" alt="${pokeName.textContent}">`;
+    types.innerHTML = ""
+    typeArr.forEach((el) => {
+        types.innerHTML += `<span class="type${el.slot} ${el.type.name}">${el.type.name.toUpperCase()}</span>`
+    });
+
     pokeStatsArr = data.stats;
-    console.log(data);
+
+    pokeStatsArr.forEach((stat, index) => {
+        statsList[index].innerHTML = pokeStatsArr[index].base_stat
+    })
+
+    console.log(pokeStatsArr)
 };
 
 const getPokemon = (input) => {
     pokemonArr.forEach((el) => {
         if (input === el.id || input === el.name) {
-            console.log(el.url);
-            hp.innerHTML = el.name.toUpperCase();
-            attack.innerHTML = el.id;
-        }
-    })
-    fetchData(input)
+            pokeName.innerHTML = el.name.toUpperCase();
+            pokeId.innerHTML = `#${el.id}`;
+        };
+    });
 };
 
 
 
 searchBtn.addEventListener("click", () => {
-    if (isPokemon(searchInput.value)) {
-        getPokemon(searchInput.value)
+    let input = searchInput.value.toLowerCase();
+    if (!isNaN(searchInput.value)) {
+        input = parseInt(input)
+    }
+    if (isPokemon(input)) {
+        getPokemon(input);
+        getPokemonStats(input);
+    } else {
+        alert("Pokémon not found")
     }
 });
